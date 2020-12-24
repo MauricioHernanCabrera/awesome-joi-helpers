aa8f90c99e435613c7623a8e571ae050bbd16fb3
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2020-12-24 15:46:53 -0300

    keys updated

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const mongoId = (Joi) => Joi.string().regex(/^[0-9a-fA-F]{24}$/);
+const mongoId = (Joi) => () => Joi.string().regex(/^[0-9a-fA-F]{24}$/);
+
+const mongoIdList = (Joi) => () => Joi.array().items(mongoId(Joi)).required();
 
 const awesomeJoiHelpers = (Joi) => {
   Joi.mongoId = mongoId(Joi);
-
-  Joi.mongoIdList = Joi.array().items(mongoId(Joi)).required();
+  Joi.mongoIdList = mongoIdList(Joi);
 
   return Joi;
 };
