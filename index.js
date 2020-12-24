1237d4260a409e3247cff7d2b25603c9b70507d1
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2020-12-24 00:24:52 -0300

    Joi fixed

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const mongoId = Joi.string().regex(/^[0-9a-fA-F]{24}$/);
+const mongoId = (Joi) => Joi.string().regex(/^[0-9a-fA-F]{24}$/);
 
 const awesomeJoiHelpers = (Joi) => {
-  Joi.mongoId = mongoId;
+  Joi.mongoId = mongoId(Joi);
 
-  Joi.mongoIdList = Joi.array().items(mongoId).required();
+  Joi.mongoIdList = Joi.array().items(mongoId(Joi)).required();
 
   return Joi;
 };
